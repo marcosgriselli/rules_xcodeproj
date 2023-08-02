d21110cd63163a7a0f930688f6c5c4d80159896a
Brentley Jones <github@brentleyjones.com>
2023-08-02 14:58:46 -0500

    WIP: Use Swift serialized debug settings
    Signed-off-by: Brentley Jones <github@brentleyjones.com>

--- tools/generators/pbxproj_prefix/test/BazelDependenciesPartialTests.swift
+++ tools/generators/pbxproj_prefix/test/BazelDependenciesPartialTests.swift
@@ -42,23 +42,6 @@ class BazelDependenciesPartialTests: XCTestCase {
 			shellScript = "\"$BAZEL_INTEGRATION_DIR/generate_bazel_dependencies.sh\"\n";
 			showEnvVarsInLog = 0;
 		};
-		FF0100000000000000000005 /* Create swift_debug_settings.py */ = {
-			isa = PBXShellScriptBuildPhase;
-			buildActionMask = 2147483647;
-			files = (
-			);
-			inputPaths = (
-				"$(BAZEL_INTEGRATION_DIR)/$(CONFIGURATION)-swift_debug_settings.py",
-			);
-			name = "Create swift_debug_settings.py";
-			outputPaths = (
-				"$(OBJROOT)/$(CONFIGURATION)/swift_debug_settings.py",
-			);
-			runOnlyForDeploymentPostprocessing = 0;
-			shellPath = /bin/sh;
-			shellScript = "perl -pe '\n  # Replace \"__BAZEL_XCODE_DEVELOPER_DIR__\" with \"$(DEVELOPER_DIR)\"\n  s/__BAZEL_XCODE_DEVELOPER_DIR__/\\$(DEVELOPER_DIR)/g;\n\n  # Replace \"__BAZEL_XCODE_SDKROOT__\" with \"$(SDKROOT)\"\n  s/__BAZEL_XCODE_SDKROOT__/\\$(SDKROOT)/g;\n\n  # Replace build settings with their values\n  s/\n    \\$             # Match a dollar sign\n    (\\()?          # Optionally match an opening parenthesis and capture it\n    ([a-zA-Z_]\\w*) # Match a variable name and capture it\n    (?(1)\\))       # If an opening parenthesis was captured, match a closing parenthesis\n  /$ENV{$2}/gx;    # Replace the entire matched string with the value of the corresponding environment variable\n\n' \"$SCRIPT_INPUT_FILE_0\" > \"$SCRIPT_OUTPUT_FILE_0\"\n";
-			showEnvVarsInLog = 0;
-		};
 		FF0100000000000000000100 /* Debug */ = {
 			isa = XCBuildConfiguration;
 			buildSettings = {BUILD_SETTINGS_HERE};
@@ -89,7 +72,6 @@ class BazelDependenciesPartialTests: XCTestCase {
 			buildConfigurationList = FF0100000000000000000002 /* Build configuration list for PBXAggregateTarget "BazelDependencies" */;
 			buildPhases = (
 				FF0100000000000000000004 /* Bazel Build */,
-				FF0100000000000000000005 /* Create swift_debug_settings.py */,
 			);
 			dependencies = (
 			);
@@ -150,23 +132,6 @@ class BazelDependenciesPartialTests: XCTestCase {
 			shellScript = "\"$BAZEL_INTEGRATION_DIR/generate_bazel_dependencies.sh\"\n";
 			showEnvVarsInLog = 0;
 		};
-		FF0100000000000000000005 /* Create swift_debug_settings.py */ = {
-			isa = PBXShellScriptBuildPhase;
-			buildActionMask = 2147483647;
-			files = (
-			);
-			inputPaths = (
-				"$(BAZEL_INTEGRATION_DIR)/$(CONFIGURATION)-swift_debug_settings.py",
-			);
-			name = "Create swift_debug_settings.py";
-			outputPaths = (
-				"$(OBJROOT)/$(CONFIGURATION)/swift_debug_settings.py",
-			);
-			runOnlyForDeploymentPostprocessing = 0;
-			shellPath = /bin/sh;
-			shellScript = "perl -pe '\n  # Replace \"__BAZEL_XCODE_DEVELOPER_DIR__\" with \"$(DEVELOPER_DIR)\"\n  s/__BAZEL_XCODE_DEVELOPER_DIR__/\\$(DEVELOPER_DIR)/g;\n\n  # Replace \"__BAZEL_XCODE_SDKROOT__\" with \"$(SDKROOT)\"\n  s/__BAZEL_XCODE_SDKROOT__/\\$(SDKROOT)/g;\n\n  # Replace build settings with their values\n  s/\n    \\$             # Match a dollar sign\n    (\\()?          # Optionally match an opening parenthesis and capture it\n    ([a-zA-Z_]\\w*) # Match a variable name and capture it\n    (?(1)\\))       # If an opening parenthesis was captured, match a closing parenthesis\n  /$ENV{$2}/gx;    # Replace the entire matched string with the value of the corresponding environment variable\n\n' \"$SCRIPT_INPUT_FILE_0\" > \"$SCRIPT_OUTPUT_FILE_0\"\n";
-			showEnvVarsInLog = 0;
-		};
 		FF0100000000000000000100 /* Debug */ = {
 			isa = XCBuildConfiguration;
 			buildSettings = {BUILD_SETTINGS_HERE};
@@ -197,7 +162,6 @@ class BazelDependenciesPartialTests: XCTestCase {
 			buildConfigurationList = FF0100000000000000000002 /* Build configuration list for PBXAggregateTarget "BazelDependencies" */;
 			buildPhases = (
 				FF0100000000000000000004 /* Bazel Build */,
-				FF0100000000000000000005 /* Create swift_debug_settings.py */,
 			);
 			dependencies = (
 			);
@@ -258,24 +222,7 @@ class BazelDependenciesPartialTests: XCTestCase {
 			shellScript = "\"$BAZEL_INTEGRATION_DIR/generate_bazel_dependencies.sh\"\n";
 			showEnvVarsInLog = 0;
 		};
-		FF0100000000000000000005 /* Create swift_debug_settings.py */ = {
-			isa = PBXShellScriptBuildPhase;
-			buildActionMask = 2147483647;
-			files = (
-			);
-			inputPaths = (
-				"$(BAZEL_INTEGRATION_DIR)/$(CONFIGURATION)-swift_debug_settings.py",
-			);
-			name = "Create swift_debug_settings.py";
-			outputPaths = (
-				"$(OBJROOT)/$(CONFIGURATION)/swift_debug_settings.py",
-			);
-			runOnlyForDeploymentPostprocessing = 0;
-			shellPath = /bin/sh;
-			shellScript = "perl -pe '\n  # Replace \"__BAZEL_XCODE_DEVELOPER_DIR__\" with \"$(DEVELOPER_DIR)\"\n  s/__BAZEL_XCODE_DEVELOPER_DIR__/\\$(DEVELOPER_DIR)/g;\n\n  # Replace \"__BAZEL_XCODE_SDKROOT__\" with \"$(SDKROOT)\"\n  s/__BAZEL_XCODE_SDKROOT__/\\$(SDKROOT)/g;\n\n  # Replace build settings with their values\n  s/\n    \\$             # Match a dollar sign\n    (\\()?          # Optionally match an opening parenthesis and capture it\n    ([a-zA-Z_]\\w*) # Match a variable name and capture it\n    (?(1)\\))       # If an opening parenthesis was captured, match a closing parenthesis\n  /$ENV{$2}/gx;    # Replace the entire matched string with the value of the corresponding environment variable\n\n' \"$SCRIPT_INPUT_FILE_0\" > \"$SCRIPT_OUTPUT_FILE_0\"\n";
-			showEnvVarsInLog = 0;
-		};
-		FF0100000000000000000006 /* Post-build Run Script */ = {POST_BUILD_HERE};
+		FF0100000000000000000005 /* Post-build Run Script */ = {POST_BUILD_HERE};
 		FF0100000000000000000100 /* Debug */ = {
 			isa = XCBuildConfiguration;
 			buildSettings = {BUILD_SETTINGS_HERE};
@@ -306,8 +253,7 @@ class BazelDependenciesPartialTests: XCTestCase {
 			buildConfigurationList = FF0100000000000000000002 /* Build configuration list for PBXAggregateTarget "BazelDependencies" */;
 			buildPhases = (
 				FF0100000000000000000004 /* Bazel Build */,
-				FF0100000000000000000005 /* Create swift_debug_settings.py */,
-				FF0100000000000000000006 /* Post-build Run Script */,
+				FF0100000000000000000005 /* Post-build Run Script */,
 			);
 			dependencies = (
 			);
@@ -369,23 +315,6 @@ class BazelDependenciesPartialTests: XCTestCase {
 			shellScript = "\"$BAZEL_INTEGRATION_DIR/generate_bazel_dependencies.sh\"\n";
 			showEnvVarsInLog = 0;
 		};
-		FF0100000000000000000005 /* Create swift_debug_settings.py */ = {
-			isa = PBXShellScriptBuildPhase;
-			buildActionMask = 2147483647;
-			files = (
-			);
-			inputPaths = (
-				"$(BAZEL_INTEGRATION_DIR)/$(CONFIGURATION)-swift_debug_settings.py",
-			);
-			name = "Create swift_debug_settings.py";
-			outputPaths = (
-				"$(OBJROOT)/$(CONFIGURATION)/swift_debug_settings.py",
-			);
-			runOnlyForDeploymentPostprocessing = 0;
-			shellPath = /bin/sh;
-			shellScript = "perl -pe '\n  # Replace \"__BAZEL_XCODE_DEVELOPER_DIR__\" with \"$(DEVELOPER_DIR)\"\n  s/__BAZEL_XCODE_DEVELOPER_DIR__/\\$(DEVELOPER_DIR)/g;\n\n  # Replace \"__BAZEL_XCODE_SDKROOT__\" with \"$(SDKROOT)\"\n  s/__BAZEL_XCODE_SDKROOT__/\\$(SDKROOT)/g;\n\n  # Replace build settings with their values\n  s/\n    \\$             # Match a dollar sign\n    (\\()?          # Optionally match an opening parenthesis and capture it\n    ([a-zA-Z_]\\w*) # Match a variable name and capture it\n    (?(1)\\))       # If an opening parenthesis was captured, match a closing parenthesis\n  /$ENV{$2}/gx;    # Replace the entire matched string with the value of the corresponding environment variable\n\n' \"$SCRIPT_INPUT_FILE_0\" > \"$SCRIPT_OUTPUT_FILE_0\"\n";
-			showEnvVarsInLog = 0;
-		};
 		FF0100000000000000000100 /* Debug */ = {
 			isa = XCBuildConfiguration;
 			buildSettings = {BUILD_SETTINGS_HERE};
@@ -417,7 +346,6 @@ class BazelDependenciesPartialTests: XCTestCase {
 			buildPhases = (
 				FF0100000000000000000003 /* Pre-build Run Script */,
 				FF0100000000000000000004 /* Bazel Build */,
-				FF0100000000000000000005 /* Create swift_debug_settings.py */,
 			);
 			dependencies = (
 			);
